4f614accbd9b0002962c2e6b15a7838af79c06bd
aptus-clayton <Clayton.b@aptus.aero>
2019-10-11 10:51:39 -0700

    basic ball movement implemented

--- objects/ball.py
+++ objects/ball.py
@@ -1,12 +1,47 @@
-from TGL.internal import GameObject, Box
+from TGL.internal import GameObject, Box, timeFunction
+
+import numpy
 
 class Ball(GameObject):
     '''
 
     '''
 
-    def __init__(self, box: Box):
+    def __init__(self, box: Box, windowSize: numpy.ndarray):
         GameObject.__init__(self, box)
     
+        self.velocity: numpy.ndarray = numpy.array([.1, .2])
+        self.lastTime: float = timeFunction()
+
+        self.position: numpy.ndarray = numpy.array([.5, .5])
+
+        self.windowSize: numpy.ndarray = windowSize
+
     def _setValues(self):
+
         self.bufferCanvas.backgroundColors.fill(255)
+    
+    def update(self):
+
+        newTime = timeFunction()
+        timeElapsed = newTime - self.lastTime
+        self.lastTime = newTime
+
+        self.position = self.position + self.velocity * timeElapsed
+
+        if self.position[0] < 0:
+            self.position[0] = -self.position[0]
+            self.velocity[0] = -self.velocity[0]
+        if self.position[1] < 0:
+            self.position[1] = -self.position[1]
+            self.velocity[1] = -self.velocity[1]
+        
+        if self.position[0] > 1.0:
+            self.position[0] = 2 - self.position[0]
+            self.velocity[0] = -self.velocity[0]
+        if self.position[1] > 1.0:
+            self.position[1] = 2 - self.position[1]
+            self.velocity[1] = -self.velocity[1]
+        
+        screenPosition = numpy.round(self.position * (self.windowSize - numpy.array([2, 1]))).astype(numpy.int16)
+        self.realX, self.realY = screenPosition[0], screenPosition[1]
--- pong.py
+++ pong.py
@@ -4,11 +4,13 @@ from objects.ball import Ball
 
 from TGL.internal import Box
 
+import numpy
+
 if __name__ == "__main__":
 
     w = Window((160, 41))
 
-    ball = Ball(Box(79, 20, 2, 1))
+    ball = Ball(Box(79, 20, 2, 1), numpy.array([160, 41]))
 
     w.addGameObject(ball)
 
